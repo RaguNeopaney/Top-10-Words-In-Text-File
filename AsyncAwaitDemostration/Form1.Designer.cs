da87cbab5c8451565c7c1d6c2934c1dc6bae42ce
Ragun <Ragun@DESKTOP-FL8CN10>
2022-06-19 12:03:40 -0400

    Add project files.

---
+++ AsyncAwaitDemostration/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace AsyncAwaitDemostration
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileLabel = new System.Windows.Forms.Label();
+            this.FilePath = new System.Windows.Forms.TextBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.MostUsedWordBox = new System.Windows.Forms.ListBox();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // FileLabel
+            // 
+            this.FileLabel.AutoSize = true;
+            this.FileLabel.Location = new System.Drawing.Point(31, 31);
+            this.FileLabel.Name = "FileLabel";
+            this.FileLabel.Size = new System.Drawing.Size(25, 15);
+            this.FileLabel.TabIndex = 0;
+            this.FileLabel.Text = "File";
+            // 
+            // FilePath
+            // 
+            this.FilePath.Location = new System.Drawing.Point(62, 28);
+            this.FilePath.Name = "FilePath";
+            this.FilePath.Size = new System.Drawing.Size(376, 23);
+            this.FilePath.TabIndex = 1;
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Location = new System.Drawing.Point(454, 28);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(91, 23);
+            this.BrowseButton.TabIndex = 2;
+            this.BrowseButton.Text = "Browse ";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // MostUsedWordBox
+            // 
+            this.MostUsedWordBox.FormattingEnabled = true;
+            this.MostUsedWordBox.ItemHeight = 15;
+            this.MostUsedWordBox.Location = new System.Drawing.Point(42, 65);
+            this.MostUsedWordBox.Name = "MostUsedWordBox";
+            this.MostUsedWordBox.Size = new System.Drawing.Size(502, 244);
+            this.MostUsedWordBox.TabIndex = 3;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(479, 325);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SubmitButton.TabIndex = 4;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(363, 325);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 5;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 374);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.MostUsedWordBox);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.FilePath);
+            this.Controls.Add(this.FileLabel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FileLabel;
+        private System.Windows.Forms.TextBox FilePath;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.ListBox MostUsedWordBox;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
---
+++ AsyncAwaitDemostration/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwaitDemostration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e) => Close();
+
+        private async void SubmitButton_Click(object sender, EventArgs e)
+        {
+            MostUsedWordBox.Items.Clear();
+
+            try
+            {
+                var file = openFileDialog.FileName;
+                List<Task> tasks = new List<Task>();
+                
+                var wordTask = ReadBook(file);
+                tasks.Add(wordTask);
+
+                await Task.WhenAll(tasks);
+                // Other operation after this finish 
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("File didn't open correctly");
+            }
+        }
+
+        private async Task ReadBook(string fileName)
+        {
+            var lines = await Task.FromResult(File.ReadAllLinesAsync(fileName));
+            var arrayLinesWithoutPunctuation =
+                lines.Result.Where(x => x != String.Empty)
+                .AsParallel().
+                Select(x => x.ToLower().Trim().Replace("x", "").Replace("'", "").Replace("{", "").Replace("}", ""));
+            var arrayOfWords = arrayLinesWithoutPunctuation.SelectMany(x => x.Split(" "));
+            Dictionary<string, int> wordDic = new Dictionary<string, int>();
+            arrayOfWords.ToList().ForEach(x =>
+            {
+                if (wordDic.ContainsKey(x))
+                {
+                    wordDic[x]++;
+                }
+                else
+                {
+                    wordDic.Add(x, 1);
+                }
+            });
+
+            var top10 = wordDic.Where(x => !string.IsNullOrWhiteSpace(x.Key)).OrderByDescending(x => x.Value).Take(10);
+            foreach (var word in top10)
+            {
+                MostUsedWordBox.Items.Add(word.Key + " - " + word.Value);
+            }
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                FilePath.Text = openFileDialog.FileName.Split("\\").LastOrDefault();
+            }
+        }
+    }
+}
